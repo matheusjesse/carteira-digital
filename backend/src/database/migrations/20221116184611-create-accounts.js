529b336ade7af1a71e37e8a0138509634865ec66
matheus jesse <matheus_jesse@hotmail.com>
2022-11-16 21:15:10 -0300

    chore: lint changes

--- backend/src/database/migrations/20221116184611-create-accounts.js
+++ backend/src/database/migrations/20221116184611-create-accounts.js
@@ -1,20 +1,18 @@
-'use strict';
-
 module.exports = {
-  async up (queryInterface, Sequelize) {
+  async up(queryInterface, Sequelize) {
     await queryInterface.createTable('accounts', {
       id: {
         type: Sequelize.INTEGER,
         primaryKey: true,
-        autoIncrement: true
+        autoIncrement: true,
       },
       balance: {
         type: Sequelize.DECIMAL,
-      }
-    })
+      },
+    });
   },
 
-  async down (queryInterface, Sequelize) {
+  async down(queryInterface, Sequelize) {
     await queryInterface.dropTable('accounts');
-  }
+  },
 };
