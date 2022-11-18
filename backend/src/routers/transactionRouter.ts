3f89e7d6efc38c5bdbc6414fd2e45e09da7e5f41
matheus jesse <matheus_jesse@hotmail.com>
2022-11-18 14:53:57 -0300

    feat: transaction router

---
+++ backend/src/routers/transactionRouter.ts
@@ -0,0 +1,23 @@
+import { Router } from 'express';
+import TransactionController from '../controllers/transactionController';
+import TransactionService from '../services/transactionService';
+import TransactionValidate from '../middlewares/transaction';
+
+const transactionService = new TransactionService();
+const transactionController = new TransactionController(transactionService);
+const transactionValidate = new TransactionValidate(transactionService);
+const router = Router();
+
+router.get(
+  '/',
+  transactionValidate.tokenValidate,
+  (req, res) => transactionController.transaction(req, res),
+);
+router.get(
+  '/filter',
+  transactionValidate.tokenValidate,
+  transactionValidate.validateBody,
+  (req, res) => transactionController.transactionFilter(req, res),
+);
+
+export default router;
