6bbf33df7dd8edc614e6b36989fe5dba40f780d6
matheus jesse <matheus_jesse@hotmail.com>
2022-11-18 14:54:36 -0300

    feat: transaction controller

---
+++ backend/src/controllers/transactionController.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from 'express';
+import ITransactiontService from '../interfaces/ITransactionService';
+import JwtService from '../middlewares/JwtService';
+
+export default class TransactionController {
+  constructor(
+    private TransactionService: ITransactiontService,
+  ) { }
+
+  async transaction(req: Request, res: Response) {
+    const token: string = req.headers.authorization as string;
+    const author = JwtService.decodeToken(token) as { username: string, password: string };
+    const transactionData = await this.TransactionService.transaction(author.username);
+    res.status(201).json(transactionData);
+  }
+
+  async transactionFilter(req: Request, res: Response) {
+    const { date, filter } = req.body;
+    const token: string = req.headers.authorization as string;
+    const author = JwtService.decodeToken(token) as { username: string, password: string };
+    const transactionData = await
+    this.TransactionService.transactionFilter(date, filter, author.username);
+    res.status(201).json(transactionData);
+  }
+}
