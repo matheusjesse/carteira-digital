cea6c00aa1508d9723d2d30bb134eb77674c6402
matheus jesse <matheus_jesse@hotmail.com>
2022-11-18 14:57:32 -0300

    feat: account controller

---
+++ backend/src/controllers/accountController.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from 'express';
+import IAccountService from '../interfaces/IAccountService';
+import JwtService from '../middlewares/JwtService';
+
+export default class AccountController {
+  constructor(
+    private AccountService: IAccountService,
+  ) { }
+
+  async balance(req: Request, res: Response) {
+    const token: string = req.headers.authorization as string;
+    const { username } = JwtService.decodeToken(token) as { username: string, password: string };
+    const user = await this.AccountService.balance(username);
+    res.status(200).json(user);
+  }
+
+  async cashOut(req: Request, res: Response) {
+    const { favorecedor, beneficiado, value } = req.body;
+    const message = await this.AccountService.cashOut(favorecedor, beneficiado, value);
+    res.status(200).json({ message });
+  }
+}
