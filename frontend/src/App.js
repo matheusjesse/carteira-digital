27fa8c4af8e137576f6103d582cfa15bbf3eae0c
matheus jesse <matheus_jesse@hotmail.com>
2022-11-20 17:09:09 -0300

    feat: adicionando rota home e register

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 import GlobalStyle from './styles/global';
 import Login from './pages/Login';
+import Home from './pages/Home';
+import Register from './pages/Register';
 
 function App() {
   return (
@@ -8,6 +10,8 @@ function App() {
       <Routes>
         <Route exact path="/login" element={<Login />} />
         <Route exact path="/" element={ <Navigate replace to="/login" /> } />
+        <Route exact path="/home" element={<Home />} />
+        <Route exact path="/register" element={<Register />} />
       </Routes>
     <GlobalStyle />
     </>
