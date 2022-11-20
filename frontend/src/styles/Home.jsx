74a81ea59d665a4d0afe5efc5fe452bc75284661
matheus jesse <matheus_jesse@hotmail.com>
2022-11-20 17:07:33 -0300

    feat: home page

---
+++ frontend/src/styles/Home.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const HomeContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  width: 100vw;
+  background: #000000;
+
+`
+
+export default HomeContainer;
