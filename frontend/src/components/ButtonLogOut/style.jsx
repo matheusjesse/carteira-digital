7dab3de7637cafb7c3422aaeddd07a98a86a50a7
matheus jesse <matheus_jesse@hotmail.com>
2022-11-20 17:07:56 -0300

    feat: componente button logout

---
+++ frontend/src/components/ButtonLogOut/style.jsx
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+const ButtonLogOutContainer = styled.form`
+  width: 520px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 8px;
+  button {
+    width: 200px;
+    height: 40px;
+    background: white;
+    border: 2px solid black;
+    border-radius: 12px;
+    box-shadow: 1px 1px 1px 1px white;
+    font-size: 1.4em;
+    cursor:pointer;
+    font-family: 'Kanit', sans-serif;
+  }
+`;
+
+export default ButtonLogOutContainer;
